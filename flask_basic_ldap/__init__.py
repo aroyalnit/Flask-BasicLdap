caf3d37c4c4fa8b1cb28b94d244959d7b9e1d22a
Ian Taylor <ian.taylor@tobias.tv>
2013-05-16 10:51:12 +0100

    Change other init args to be optional / definable later

--- flask_basic_ldap/__init__.py
+++ flask_basic_ldap/__init__.py
@@ -3,7 +3,7 @@ from functools import wraps
 from flask import request, Response
 
 class BasicLDAPAuth:
-    def __init__(self, ldap_basedn, ldap_userfilter, ldap_uri=''):
+    def __init__(self, ldap_basedn='', ldap_userfilter='', ldap_uri=''):
         self.ldap_basedn = ldap_basedn
         self.ldap_userfilter = ldap_userfilter
         self.ldap_uri = ldap_uri
